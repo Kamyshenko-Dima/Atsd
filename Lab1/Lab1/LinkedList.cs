959edaacd1d81f20825cf7fb209d9422c9ba1d29
KamyshDima <dimakamyshenko077@gmail.com>
2021-03-30 17:25:55 +0300

    Added PrintList method

--- Lab1/Lab1/LinkedList.cs
+++ Lab1/Lab1/LinkedList.cs
@@ -4,6 +4,7 @@ namespace Lab1
 {
     class LinkedList
     {
+        Node head;
         class Node
         {
             public int Data;
@@ -16,9 +17,28 @@ namespace Lab1
                 Next = null;
             }
         }
+
+        public void PrintList()
+        {
+            Node n = head;
+            while(n != null)
+            {
+                Console.Write(n.Data + " ");
+                n = n.Next;
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList llist = new LinkedList();
+            llist.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+
+            llist.head.Next = second;
+            second.Next = third;
+
+            llist.PrintList();
         }
     }
 }
