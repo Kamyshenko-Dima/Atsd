f883881e54f3884c0785525b36177f46e5781965
KamyshDima <dimakamyshenko077@gmail.com>
2021-03-30 17:17:51 +0300

    Added class node

---
+++ Lab1/Lab1/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab1
+{
+    class LinkedList
+    {
+        class Node
+        {
+            public int Data;
+            public Node Next;
+
+            //constructor
+            public Node(int d)
+            {
+                Data = d;
+                Next = null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
